169bc9d4cae7ba7a370095cb28986633560a1e73
Oleg Buravenko <oleg_burav@ukr.net>
2023-11-04 20:36:10 +0200

    lab4

--- main.py
+++ main.py
@@ -5,23 +5,23 @@ from pyspark.sql.functions import expr
 spark = SparkSession.builder.appName("praktuchna3").getOrCreate()
 df = spark.read.csv("data/trip_data_1.csv", header=True, inferSchema=True)
 
-min_longitude = -74.05
-max_longitude = -73.75
+min_latitude = 40.6
+max_latitude = 40.9
 
-filtered_df = df.filter((df['pickup_longitude'] >= min_longitude) & (df['pickup_longitude'] <= max_longitude) &
-                        (df['dropoff_longitude'] >= min_longitude) & (df['dropoff_longitude'] <= max_longitude))
+filtered_df = df.filter((df['pickup_latitude'] >= min_latitude) & (df['pickup_latitude'] <= max_latitude) &
+                        (df['dropoff_latitude'] >= min_latitude) & (df['dropoff_latitude'] <= max_latitude))
 
 filtered_df = filtered_df.filter((df['trip_distance'] >= 0) & (df['trip_distance'] <= 2))
 
-filtered_df = filtered_df.withColumn("longitude_difference", expr("pickup_longitude - dropoff_longitude"))
+filtered_df = filtered_df.withColumn("latitude_difference", expr("pickup_latitude - dropoff_latitude"))
 
-grouped_df = df.groupBy("pickup_longitude")
+grouped_df = filtered_df.groupBy("pickup_latitude")
 
-joined_df = df.join(grouped_df, 'medallion', 'inner')
+joined_df = filtered_df.join(grouped_df, "pickup_latitude", "inner")
 
-joined_df = joined_df.withColumn("longitude_difference", expr("pickup_longitude - dropoff_longitude"))
+joined_df = joined_df.withColumn("latitude_difference", expr("pickup_latitude - dropoff_latitude"))
 
-df = joined_df.filter((df['longitude_difference'] > 0.1))
+filtered_df = joined_df.filter((joined_df['latitude_difference'] > 0.1))
 
-df.show()
+filtered_df.show()
 spark.stop()
