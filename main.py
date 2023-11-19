fb51f476b5bbfd69e3865ea69b52e58b13fa0624
Solomiia Kreminets <miiakreminets@gmail.com>
2023-11-19 22:05:30 +0200

    perform different operations using pyspark

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 from pyspark.sql import SparkSession
-import transformation_stage
+from pyspark.sql.functions import rank, sum, count, expr, min, max, expr
 
-# Create a SparkSession
-spark = SparkSession.builder \
-    .appName("PySparkTest") \
-    .getOrCreate()
+spark = SparkSession.builder.appName("praktuchna3").getOrCreate()
+df = spark.read.csv("data/trip_data_1.csv", header=True, inferSchema=True)
 
-# Create a test DataFrame
-data = [("Alice", 34), ("Bob", 45), ("Charlie", 29)]
-schema = ["name", "age"]
-df = spark.createDataFrame(data, schema)
+# Question: What is the average rate score for trips with more than 2 passengers?
+filtered_df_2 = df.filter((df['passenger_count'] > 2))
+result_df_q1 = filtered_df_2.groupBy('medallion').agg(expr('avg(rate_score)').alias('average_rate_score'), expr('count(rate_score)').alias('total_trips_with_more_than_two_passengers'))
 
-# Display the DataFrame
+# Question: What is the total number of trips and the average rate score for each passenger count?
+result_df_q2 = df.groupBy('passenger_count').agg(expr('count(medallion)').alias('total_trips'), expr('avg(rate_score)').alias('average_rate_score'))
+
+# Question: For each medallion, what is the difference between the maximum and minimum rate scores?
+result_df_q3 = df.groupBy('medallion').agg((expr('max(rate_score)') - expr('min(rate_score)')).alias('rate_score_difference'))
+
+# Question: How many passengers had a trip with a rate that doesn't equal 1?
+result_df_additional = df.filter(df['rate_score'] != 1).agg(expr('sum(passenger_count)').alias('total_passengers'))
+
+result_df_additional.show()
+
 df.show()
-
-# Stop the SparkSession (important to release resources)
 spark.stop()
-
-if __name__ == '__main__':
-    transformation_stage.run()
